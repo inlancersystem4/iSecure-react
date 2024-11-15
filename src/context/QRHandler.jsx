9578e5accf5693cccc8855bec6dd35b045eccd17
inlancersystem4 <inlancersystem4@gmail.com>
2024-11-16 02:00:48 +0530

    update logic

--- src/context/QRHandler.jsx
+++ src/context/QRHandler.jsx
@@ -1,18 +1,20 @@
 import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
-import LayoutWrapper from "../components/ui/layout/LayoutWrapper";
+import { useState, useEffect } from "react";
+import { Scanner } from "@yudiel/react-qr-scanner";
 import { toast } from "sonner";
+import LayoutWrapper from "../components/ui/layout/LayoutWrapper";
 import { post } from "../utils/apiHelper";
 import { setQrToken, setQrDetail } from "../redux/actions/actions";
 
 const QRHandler = ({ children }) => {
   const [token, setToken] = useState(null);
   const [qrFetchSuccess, setQRFetchSuccess] = useState(true);
+  const [scanQROn, setScanQROn] = useState(false);
 
   async function qrDetailsFetch(token) {
     try {
       const response = await post("/qr-details", { token: token });
-      if (response.success == 1) {
+      if (response.success === 1) {
         setQrDetail({
           gate: response.data.gate,
           apartment: response.data.apartment,
@@ -26,25 +28,69 @@ const QRHandler = ({ children }) => {
       }
     } catch (e) {
       setQrToken("");
-      console.error("Error apartment data", e);
+      // setQRFetchSuccess(false);
+      console.error("Error fetching QR details", e);
     }
   }
+
+  const handleScan = (data) => {
+    const rawValue = data[0].rawValue;
+    console.log(rawValue);
+
+    if (rawValue) {
+      const decodedUrl = decodeURIComponent(rawValue);
+
+      const urlToken = new URLSearchParams(data).get("token");
+
+      if (urlToken) {
+        setToken(urlToken);
+
+        const url = new URL(decodedUrl);
+        const domain = url.hostname;
+
+        if (domain !== import.meta.env.VITE_DOMAIN) {
+          toast.error(
+            "Error: The URL domain does not match the expected domain."
+          );
+        }
+      } else {
+        toast.error("Error: No token found in the URL.");
+      }
+    } else {
+      toast.error("Error: Raw value is empty.");
+    }
+  };
+
+  const handleError = (err) => {
+    console.error("QR Scan Error:", err);
+  };
 
   useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search);
     const urlToken = queryParams.get("token");
-    setToken(urlToken);
+    if (urlToken) {
+      setToken(urlToken);
+    }
   }, []);
 
   useEffect(() => {
     if (token) {
       qrDetailsFetch(token);
+      setScanQROn(false);
+    } else {
+      setScanQROn(true);
     }
   }, [token]);
 
   return (
     <LayoutWrapper>
-      {qrFetchSuccess ? <div>{children}</div> : <div className="">Loading</div>}
+      {scanQROn ? (
+        <Scanner delay={300} onError={handleError} onScan={handleScan} />
+      ) : qrFetchSuccess ? (
+        <div>{children}</div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </LayoutWrapper>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,121 @@
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
+import Webcam from "react-webcam";
 import { ReactMic } from "react-mic";
-import { ArrowLeft, QrCode, RotateCcw } from "lucide-react";
+import {
+  ArrowLeft,
+  QrCode,
+  RotateCcw,
+  Mic,
+  CircleStop,
+  Aperture,
+} from "lucide-react";
 
 export default function HomePage() {
+  const webcamRef = useRef(null);
+
   const [record, setRecord] = useState(false);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [questions, setQuestions] = useState([
+    { id: 0, answer_file: "" },
+    { id: 1, question: "What is your Name?", answer_file: "" },
+    { id: 2, question: "Flat Owner Name?", answer_file: "" },
+    { id: 3, question: "Flat Number or Flat Owner Name?", answer_file: "" },
+    { id: 4, question: "Reason of Visit?", answer_file: "" },
+  ]);
 
-  const handleStart = () => {
-    setRecord(true);
-  };
+  const changeQuestionIndex = useCallback(
+    (increment) => {
+      setQuestionIndex((prevIndex) => {
+        const nextIndex = prevIndex + increment;
+        if (nextIndex >= 0 && nextIndex < questions.length) {
+          return nextIndex;
+        }
+        return prevIndex;
+      });
+    },
+    [questions.length]
+  );
 
-  const handleStop = () => {
-    setRecord(false);
-  };
+  const handleToggleRecording = useCallback(() => {
+    setRecord((prev) => !prev);
+  }, []);
 
-  const onStop = (recordedBlob) => {
-    console.log("Recorded Blob:", recordedBlob);
+  const onStop = useCallback(
+    (file) => {
+      setQuestions((prevQuestions) => {
+        const updatedQuestions = [...prevQuestions];
+        updatedQuestions[questionIndex].answer_file = file;
+        console.log(updatedQuestions);
+        return updatedQuestions;
+      });
+
+      changeQuestionIndex(1);
+    },
+    [questionIndex, changeQuestionIndex]
+  );
+
+  const handleResetQuestions = useCallback(() => {
+    setQuestionIndex(0);
+  }, []);
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setQuestions((prevQuestions) => {
+      const updatedQuestions = [...prevQuestions];
+      updatedQuestions[questionIndex].image_answer = imageSrc;
+      return updatedQuestions;
+    });
+    changeQuestionIndex(1);
+  }, [webcamRef, questionIndex, changeQuestionIndex]);
+
+  const renderQuestionContent = () => {
+    const currentQuestion = questions[questionIndex];
+    if (currentQuestion.id === 0) {
+      return (
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          width="100%"
+          videoConstraints={{ facingMode: "user" }}
+          onUserMediaError={() => console.log("Error accessing camera")}
+          onScreenshot={(imageData) => onStop(imageData)}
+        />
+      );
+    }
+
+    return (
+      <>
+        <h3>{currentQuestion.question}</h3>
+        {currentQuestion.audio_answer && (
+          <audio controls>
+            <source src={currentQuestion.audio_answer} />
+            Your browser does not support the audio element.
+          </audio>
+        )}
+        {currentQuestion.image_answer && (
+          <img
+            src={currentQuestion.image_answer}
+            alt="Captured"
+            style={{ width: "100%", height: "auto" }}
+          />
+        )}
+      </>
+    );
   };
 
   return (
     <div className="full-page">
       <div className="header">
-        <button className="btn">
-          <ArrowLeft size={18} />
-        </button>
+        {questionIndex != 0 ? (
+          <button className="btn" onClick={() => changeQuestionIndex(-1)}>
+            <ArrowLeft size={18} />
+          </button>
+        ) : (
+          <div></div>
+        )}
         <div className="btn-group">
-          <button className="btn">
+          <button className="btn" onClick={handleResetQuestions}>
             <RotateCcw size={18} />
           </button>
           <button className="btn">
@@ -32,22 +123,31 @@ export default function HomePage() {
           </button>
         </div>
       </div>
-      <div className="body"></div>
+
+      <div className="body">{renderQuestionContent()}</div>
+
       <div className="footer">
-        <ReactMic
-          record={record}
-          onStop={onStop}
-          strokeColor="#FF0000"
-          backgroundColor="#000000"
-        />
-        <div className="controls">
-          <button className="btn" onClick={handleStart}>
-            Start Recording
-          </button>
-          <button className="btn" onClick={handleStop}>
-            Stop Recording
-          </button>
-        </div>
+        {questionIndex == 0 ? (
+          <div className="controls">
+            <button className="btn" onClick={capture}>
+              <Aperture />
+            </button>
+          </div>
+        ) : (
+          <>
+            <ReactMic
+              record={record}
+              onStop={onStop}
+              strokeColor="#FF0000"
+              backgroundColor="#000000"
+            />
+            <div className="controls">
+              <button className="btn" onClick={handleToggleRecording}>
+                {record ? <CircleStop /> : <Mic />}
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
